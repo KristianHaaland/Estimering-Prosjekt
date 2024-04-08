9e0ad9980858ebaa3118ac96b69e7898eadc98d3
KristianHaaland <Kristianhaaland01@gmail.com>
2024-04-08 14:53:53 +0200

    Lese inn fil i python script

--- iris_task.py
+++ iris_task.py
@@ -7,10 +7,24 @@ import matplotlib.pyplot as plt
 def read_file(txt_file):
     with open(txt_file, 'r') as file:
 
-        content = file.read()
+        data = file.readlines()
 
-        content_list = content.split()
+        iris_data = []
 
-        content_list = [float(value) for value in content_list]
+        #Append each line from data in the iris_data to create an array
+        for line in data:
+                iris_data.append([float(num) for num in line.split(',')])
+
+        #Splitting into training and test data
+        training_data = iris_data[:30]
+        test_data = iris_data[30:]
   
-    return np.array(content_list)
+    return training_data, test_data
+
+class_1_training, class_1_test = read_file('class_1')
+class_2_training, class_2_test = read_file('class_2')
+class_3_training, class_3_test = read_file('class_3')
+
+#The 
+def sigmoid(z):
+     return 1/(1+np.e(-z))
