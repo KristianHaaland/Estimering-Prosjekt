8605bdb670e1cc2697ae89835410e5c2d3d5b9ed
KristianHaaland <Kristianhaaland01@gmail.com>
2024-04-09 15:22:46 +0200

    Iris nesten ferdig koda

--- iris_task.py
+++ iris_task.py
@@ -19,52 +19,138 @@ def read_file(txt_file):
         training_data = iris_data[:30]
         test_data = iris_data[30:]
   
-    return training_data, test_data
+    return iris_data, training_data, test_data
 
-class_1_training, class_1_test = read_file('class_1')
-class_2_training, class_2_test = read_file('class_2')
-class_3_training, class_3_test = read_file('class_3')
+class_1, class_1_training, class_1_test = read_file('class_1')
+class_2, class_2_training, class_2_test = read_file('class_2')
+class_3, class_3_training, class_3_test = read_file('class_3')
+training_data = np.concatenate((class_1_training, class_2_training, class_3_training), axis=0)
+test_data = np.concatenate((class_1_test, class_2_test, class_3_test), axis=0)
 
-
-#z=Wx
 def sigmoid(z):
-     return 1/(1+np.e(-z))
+     return 1/(1+np.exp(-z))
+
+def g_k(W, x_k):
+     z = np.dot(W, x_k)
+     return sigmoid(z)
 
 #Initializing the weigth matrix. Each element is initially random between 0 and 0.01
 W = np.random.rand(3, 4) * 0.01
 
-def g_k(W, x):
-     return np.dot(W, x)
-
 #g_k er estimert verdi fra overnevnte funksjon
 #t_k er target vector. Så om input x_k er av class 1, vil t_k=[1, 0, 0]
 #x_k er input vektoren
-def grad_MSE(g, t, x):
+def grad_MSE(W, training_data):
      
-     sum_elements = 0
+     #Initial values for MSE and its gradient matrix
+     grad_MSE_matrix = np.zeros((3, 4))
+     MSE = 0
 
-     for i in range(len(class_1_training)-1):
-          sum_elements += (g[i]-t[i])*g[i]*(1-g[i])*np.transpose(x[i])
+     #Checks which class the target output is
+     for k in range(len(training_data)-1):
+          if k < 30:
+               t = np.array([1, 0, 0])
+          elif 30 <= k < 60:
+               t = np.array([0, 1, 0])
+          else:
+               t = np.array([0, 0, 1])
 
-     return sum_elements
+          #Computing values for g, x_k and column vector used in outer product
+          g = g_k(W, training_data[k])
+          x_k = training_data[k].reshape(1, -1)
+          col_vector = ((g - t) * g * (1 - g)).reshape(-1, 1)
+
+          #Computing the MSE and its gradient
+          MSE += 0.5 * np.dot((g-t).T, g-t)
+          grad_MSE_matrix += np.dot(col_vector, x_k)
+
+     return grad_MSE_matrix, MSE
+
+
+def training(W, training_data):
+     
+     #Initial values used in training
+     alpha = .002
+     MSE = grad_MSE(W, training_data)[1]
+     iteration = 0
+
+     #Training until the MSE reaches disired threshold
+     while MSE > 9:
+
+          #Iterating the training data for current W matrix
+          for i in range(len(training_data)):
+               #Updating the W matrix
+               W -= alpha * grad_MSE(W, training_data)[0]
+
+          #Updating the MSE     
+          MSE = grad_MSE(W, training_data)[1]
+          print(MSE)
+
+          iteration += 1
+     #print(iteration)
+
+     return W
+
+#print(training(W, training_data))
+
+def test(W, test_data):
+         
+         for x_k in test_data:
+              g = g_k(W, x_k)
+              print(np.argmax(g))
+
+#W_trained = training(W, training_data)
+#test(W_trained, test_data)
+
+
+
+
+#Til neste gang:
+
+#Sjekke om det er klassifisert riktig eller ikke
+#Lage confussion matrix
+#Finne error rate
+#Bytte mengde for trening og test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #Scatter plots
-def plot(training_data):
+# def plot(training_data, legend_label):
      
-     sepal_lengths = [elem[0] for elem in training_data]
-     sepal_widths = [elem[1] for elem in training_data]
-     petal_lengths = [elem[2] for elem in training_data]
-     petal_widths = [elem[3] for elem in training_data]
+#      sepal_lengths = [elem[0] for elem in training_data]
+#      sepal_widths = [elem[1] for elem in training_data]
+#      petal_lengths = [elem[2] for elem in training_data]
+#      petal_widths = [elem[3] for elem in training_data]
 
-     plt.xlabel('Sepal Length')
-     plt.ylabel('Sepal Width')
-     plt.grid()
-     #plt.title()
+#      plt.xlabel('Sepal Length')
+#      plt.ylabel('Sepal Width')
+#      plt.grid()
+#      #plt.title()
 
-     return plt.scatter(petal_lengths, petal_widths)
+#      return plt.scatter(petal_lengths, petal_widths, label = legend_label)
 
-#plot(class_1)
-#plot(class_2)
-#plot(class_3)
-#plt.show()
+# plot(class_1, 'Iris-setosa')
+# plot(class_2, 'Iris-vercicolor')
+# plot(class_3, 'Iris-virginica')
+# plt.legend()
+# plt.show()
